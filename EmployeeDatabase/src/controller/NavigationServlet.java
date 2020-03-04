7a6999a464168396b15fcbc1206f6d33fee1f937
Omar <gomar@dmacc.edu>
2020-03-04 15:45:07 -0600

    commit navigationServlet
    Added navigationServlet to controller package.
    
    Please,check,

---
+++ EmployeeDatabase/src/controller/NavigationServlet.java
@@ -0,0 +1,73 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.EmployeeList;
+
+/**
+ * Servlet implementation class NavigationServlet
+ */
+@WebServlet("/navigationServlet")
+public class NavigationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NavigationServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		EmloyeeListHelper dao = new EmloyeeListHelper();
+		String act = request.getParameter("doThisToEmployee");
+		String path = "/viewAllEmployeesServlet";
+
+		if (act.equals("delete")) {
+			try {
+				Integer tempId = Integer.parseInt(request.getParameter("id"));
+				EmployeeList empToDelete = dao.searchForEmpById(tempId);
+				dao.deleteEmp(empToDelete);
+
+			} catch (NumberFormatException e) {
+				System.out.println(" select an employee");
+			}
+
+		} else if (act.equals("edit")) {
+			try {
+				Integer tempId = Integer.parseInt(request.getParameter("id"));
+				EmployeeList empToEdit = dao.searchForEmpById(tempId);
+				request.setAttribute("emToEdit", empToEdit);
+				path = "/edit_employee.jsp";
+			} catch (NumberFormatException e) {
+				System.out.println("select an employee");
+			}
+
+		} else if (act.equals("add")) {
+			path = "/index.html";
+
+		}
+
+		getServletContext().getRequestDispatcher(path).forward(request, response);
+
+	}
+
+}
