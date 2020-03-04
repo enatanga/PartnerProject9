2d94bc83d0ce924ccce4b60da9b66fc71cbfaea0
Omar <gomar@dmacc.edu>
2020-03-04 16:09:59 -0600

    commit ViewAllEmployeesServlet.java
    Added ViewAllEmployeesServlet.java controller package.

---
+++ EmployeeDatabase/src/controller/ViewAllEmployeesServlet.java
@@ -0,0 +1,54 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ViewAllEmployeesServlet
+ */
+@WebServlet("/viewAllEmployeesServlet")
+public class ViewAllEmployeesServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ViewAllEmployeesServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		EmloyeeListHelper dao = new EmloyeeListHelper();
+		
+		request.setAttribute("allEmps", dao.showAllEmp());
+		
+		String path = "/employee_list.jsp";
+		
+		if(dao.showAllEmp().isEmpty()){
+			//if there's nothing in the list, redirect the user to the index to add an item
+			path = "/index.html";
+		
+		}
+
+		getServletContext().getRequestDispatcher(path).forward(request, response);
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
