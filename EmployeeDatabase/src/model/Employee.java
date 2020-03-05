c53d1209878f83e7313990174e3332177aad7efa
Manny A <54607961+eatanga@users.noreply.github.com>
2020-03-05 15:25:21 -0600

    Update Employee.java

--- EmployeeDatabase/src/model/Employee.java
+++ EmployeeDatabase/src/model/Employee.java
@@ -1,11 +1,25 @@
 package model;
 
 import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="employees")
 public class Employee {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="ID")
 	private int id;
-	private int name;
+	@Column(name="DEPARMENT")
 	private int department;
+	@Column(name="EMPLOYEE")
+	private int employee;
+	@Column(name="TRIP_DATE")
 	private LocalDate hireDate;
 	
 	//Default no arg constructor
@@ -13,46 +27,51 @@ public class Employee {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	//parameterized constructor
-	public Employee(int id, int name, int department, LocalDate hireDate) {
+
+	public Employee(int id, int department, int employee, LocalDate hireDate) {
 		super();
 		this.id = id;
-		this.name = name;
 		this.department = department;
+		this.employee = employee;
 		this.hireDate = hireDate;
 	}
-	//Getters and setters
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
-	public int getName() {
-		return name;
-	}
-	public void setName(int name) {
-		this.name = name;
-	}
+
 	public int getDepartment() {
 		return department;
 	}
+
 	public void setDepartment(int department) {
 		this.department = department;
 	}
+
+	public int getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(int employee) {
+		this.employee = employee;
+	}
+
 	public LocalDate getHireDate() {
 		return hireDate;
 	}
+
 	public void setHireDate(LocalDate hireDate) {
 		this.hireDate = hireDate;
 	}
-	
-	
+
 	@Override
 	public String toString() {
-		return "Employee [id=" + id + ", name=" + name + ", department=" + department + ", hireDate=" + hireDate + "]";
+		return "Employee [id=" + id + ", department=" + department + ", employee=" + employee + ", hireDate=" + hireDate
+				+ "]";
 	}
-	
-	
 	
 }
