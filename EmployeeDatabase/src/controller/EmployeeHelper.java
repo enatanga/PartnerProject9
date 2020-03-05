7153f17efd0ca0363085f1a7ce7915825f771c1f
Manny A <54607961+eatanga@users.noreply.github.com>
2020-03-05 16:41:17 -0600

    Update EmployeeHelper.java

--- EmployeeDatabase/src/controller/EmployeeHelper.java
+++ EmployeeDatabase/src/controller/EmployeeHelper.java
@@ -7,36 +7,98 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 
-import model.Department;
+import model.Employee;
+
 
 public class EmployeeHelper {
 
-	static EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("EmployeeDatabase");
+	static EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("employeedata");
 
-	public void insertEmployee(Department s) {
+	public void insertItem(Employee empl) {
 		EntityManager em = emfactory.createEntityManager();
 		em.getTransaction().begin();
-		em.persist(s);
+		em.persist(empl);
 		em.getTransaction().commit();
 		em.close();
 	}
-	public List<Department> showAllEmployee() {
+
+	public List<Employee> showAllEmployee() {
 		EntityManager em = emfactory.createEntityManager();
-		List<Department> allEmployee = em.createQuery("SELECT s FROM Department s").getResultList();
+		List<Employee> allEmployee = em.createQuery("SELECT i FROM Employee i").getResultList();
 		return allEmployee;
 	}
-	public Department searchForEmployeeByName(String deportmentName) {
+
+	public void deleteItem(Employee toDelete) {
 		// TODO Auto-generated method stub
 		EntityManager em = emfactory.createEntityManager();
 		em.getTransaction().begin();
-		TypedQuery<Department> typedQuery = em.createQuery("select s from Department s where s.deportmentName = :selectedName", Department.class);
-		typedQuery.setParameter("selectedName", deportmentName);
+		TypedQuery<Employee> typedQuery = em.createQuery(
+				"select empl from Employee empl where empl.department = :selectedDepartment and empl.employee = :selectedEmployee",
+				Employee.class);
+		// Substitute parameter with actual data from the toDelete item
+		typedQuery.setParameter("selectedDepartment", toDelete.getDepartment());
+		typedQuery.setParameter("selectedEmployee", toDelete.getEmployee());
+
+		// we only want one result
 		typedQuery.setMaxResults(1);
 
-		Department found = typedQuery.getSingleResult();
+		// get the result and save it into a new list item
+		Employee result = typedQuery.getSingleResult();
+
+		// remove it
+		em.remove(result);
+		em.getTransaction().commit();
+		em.close();
+
+	}
+
+	public Employee searchForEmployeeById(int idToEdit) {
+		// TODO Auto-generated method stub
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+		Employee found = em.find(Employee.class, idToEdit);
 		em.close();
 		return found;
 	}
+
+	public void updateItem(Employee toEdit) {
+		// TODO Auto-generated method stub
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+
+		em.merge(toEdit);
+		em.getTransaction().commit();
+		em.close();
+	}
+
+	public List<Employee> searchForEmployeeByStore(String departmentName) {
+		// TODO Auto-generated method stub
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+		TypedQuery<Employee> typedQuery = em.createQuery("select empl from Employee empl where empl.department = :selectedDepartment",
+				Employee.class);
+		typedQuery.setParameter("selectedDepartment", departmentName);
+
+		List<Employee> foundItems = typedQuery.getResultList();
+		em.close();
+		return foundItems;
+	}
+
+	public List<Employee> searchForEmployeeByItem(String employeeName) {
+		// TODO Auto-generated method stub
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+		TypedQuery<Employee> typedQuery = em.createQuery("select empl from Employee empl where empl.employee = :selectedEmployee",
+				Employee.class);
+		typedQuery.setParameter("selectedEmployee", employeeName);
+
+		List<Employee> foundEmployee = typedQuery.getResultList();
+		em.close();
+		return foundEmployee;
+	}
+
+	public void cleanUp() {
+		emfactory.close();
+	}
+
 }
-
-
